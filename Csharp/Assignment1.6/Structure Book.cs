68cb07c1de7f1e342c5b99a53c8ed16ba1fddaf5
dsh145002 <76961116+dsh145002@users.noreply.github.com>
2022-09-08 00:24:56 +0530

    Assignment1 Q6

---
+++ Csharp/Assignment1.6/Structure Book.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment1._6
+{
+    struct Book
+    {
+        int BookId;
+        string Title;
+        double Price;
+        enum Type
+        {
+            MAGAZINE,
+            NOVEL,
+            REFERENCEBOOK,
+            MISCELLANEOUS
+        }
+        Type BookType;
+
+        public Book(int bookId, string Title, double Price, string BookType)
+        {
+            this.BookId = bookId;
+            this.Title = Title;
+            this.Price = Price; ;
+            this.BookType = (Type)Enum.Parse(typeof(Type), BookType);
+        }
+        public void printDetails()
+        {
+            Console.WriteLine("BookId = {0}\tBook Title = {1}\tPrice of Book = {2}\tType of Book = {3}",
+                    BookId, Title, Price, Enum.GetName(typeof(Type), BookType));
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <Book> books = new List<Book>();
+            string choice;
+            string cont = "Y";
+            do
+            {
+
+                Console.WriteLine("Press 1 for Add Book/s\nPress 2 to Display Detials\nEnter your choice: ");
+                choice = (Console.ReadLine());
+                
+                switch (choice)
+                {
+                    case "1":
+                        
+                        Console.WriteLine("Enter BookId : ");
+                        int bookId = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Book Title : ");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Enter Book Price : ");
+                        double price = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Enter Book Type : ");
+                        string type = (Console.ReadLine()).ToUpper();
+                        books.Add(new Book(bookId,name,price,type));
+                        break;
+                    case "2":
+                        foreach( Book b in books)
+                        {
+                            b.printDetails();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                Console.WriteLine("Do you want to continue Y/N :");
+                cont = Console.ReadLine().ToUpper();
+            } while (cont == "Y");
+        }
+    }
+}
